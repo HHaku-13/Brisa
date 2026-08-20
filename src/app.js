9942095e11cf65288712d300fadd8a1a1953a22d
HHaku-13 <zazachoco59@gmail.com>
2026-08-20 14:09:09 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -107,6 +107,7 @@ class TitanBot extends Client {
       );
       
       this.setupCronJobs();
+      startTwitchChecker(this);
     } catch (error) {
       logger.error('Failed to start bot:', error);
       process.exit(1);
